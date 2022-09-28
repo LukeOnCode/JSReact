a590fff294e5c83c5d926376eb4d4fd886078d68
LukeOnCode <LukasPetrylaEmail@gmail.com>
2022-09-28 18:13:33 +0200

    resolve issue on login

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -42,40 +42,47 @@ router.post(
         }
         
         try {
+            console.log('BEFORE FOR CYCLE');
             for (const key in users.users) {
                 const prop = users.users[key];
+                console.log(`${prop.email} THIS IS EMAIL`);
                 if(req.body.email === prop.email){
                     let user = match(users.users, req.body.email)
+                    console.log(`MATCHED USER -> ${user}`);
                     let pwd_ok = await bcrypt.compare(req.body.password, users.users[user].pwd)
                     
                     if(!pwd_ok){
                         res.status(400).json({ msg: "wrong credentials" })
                     }
-                    const payload = {
-                        user: {
-                            id: users.users[user].id,
-                            email: users.users[user].email,
-                            name: users.users[user].name,
-                            date: users.users[user].date,
-                            avatar: users.users[user].avatar
+                    if(pwd_ok){
+                        const payload = {
+                            user: {
+                                id: users.users[user].id,
+                                email: users.users[user].email,
+                                name: users.users[user].name,
+                                date: users.users[user].date,
+                                avatar: users.users[user].avatar
+                            }
                         }
+                        jwt.sign(
+                            payload,
+                            config.get('jwtSecret'),
+                            { expiresIn: 360000 },
+                            ( err, token ) => {
+                                if(err) throw err;
+                                res.json({ token })
+                            }
+                        );
                     }
-                    jwt.sign(
-                        payload,
-                        config.get('jwtSecret'),
-                        { expiresIn: 360000 },
-                        ( err, token ) => {
-                            if(err) throw err;
-                            res.json({ token })
-                        }
-                    );
+                }
+                if(req.body.email !== prop.email){
+                    res.status(400).json({ msg: "wrong credentials" })
                 }
             }
+            console.log('AFTER FOR CYCLE');
         } catch (error) {
             console.log(error);
         }
     }
 )
-
-
 module.exports = router;
