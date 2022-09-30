596dbea4b14f6043fe6b83eb94926801b87d53b8
LukeOnCode <LukasPetrylaEmail@gmail.com>
2022-09-30 15:57:14 +0200

    add react to project

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const {deleteObjectFile ,change} = require('./utils/object_utils')
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 //Init middleware
 app.use(express.json({ extended: false }))
 
