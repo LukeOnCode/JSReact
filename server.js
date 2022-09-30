e04f6357b9c3ad4a839c52df243855755f449b81
LukeOnCode <LukasPetrylaEmail@gmail.com>
2022-09-30 15:45:16 +0200

    add post router

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ app.get('/', (req, res) => res.send('API running'));
 app.use('/api/users', require('./routes/api/user'))
 app.use('/api/auth', require('./routes/api/auth'))
 app.use('/api/profile', require('./routes/api/profile'))
+app.use('/api/post', require('./routes/api/post'))
 
 app.listen(PORT, () => {
     deleteObjectFile();
