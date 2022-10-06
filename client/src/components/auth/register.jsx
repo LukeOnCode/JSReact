eb0dea6cf2771de2429565c3c480772d6bff05e3
LukeOnCode <LukasPetrylaEmail@gmail.com>
2022-10-06 12:20:42 +0200

    edit register form

--- client/src/components/auth/register.jsx
+++ client/src/components/auth/register.jsx
@@ -1,16 +1,31 @@
 import React, {Component} from "react";
 import {Form, Button} from 'react-bootstrap';
+
 export default class Register extends Component{
-    constructor(props){
-        super(props);
+
+    
+    state = {
+        name: '',
+        email: ''
     }
 
+    reg = (e) => this.setState({...this.state, [e.target.name]: e.target.value})
+    send = (e) => {
+        e.preventDefault();
+        console.log(this.state.email);
+    }
     render(){
+
         return(
         <Form>
             <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
+                <Form.Control onChange={this.reg } name='name' type="text" placeholder="Enter name" />
+            </Form.Group>
+            
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control onChange={this.reg}  name='email' type="email" placeholder="Enter email" />
                 <Form.Text className="text-muted">
                 We'll never share your email with anyone else.
                 </Form.Text>
@@ -18,12 +33,18 @@ export default class Register extends Component{
 
             <Form.Group className="mb-3" controlId="formBasicPassword">
                 <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
+                <Form.Control name='password' type="password" placeholder="Password" />
             </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                <Form.Check type="checkbox" label="Check me out" />
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control name='password2' type="password" placeholder="Password" />
+                <Form.Text className="text-muted">
+                    Insert password again
+                </Form.Text>
             </Form.Group>
-            <Button variant="primary" type="submit">
+
+            <Button onClick={this.send} variant="primary">
                 Submit
             </Button>
         </Form>
